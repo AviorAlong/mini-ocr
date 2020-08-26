c0dd00f6ab7107135b76fd9aadbe07b453231b2f
shial <shial@example.com>
2020-08-26 18:19:07 +0800

    v1.2

--- index.js
+++ index.js
@@ -1,150 +1,149 @@
 const debug = require('electron-debug')
 const path = require('path')
 const {
-  app,
-  globalShortcut,
-  BrowserWindow,
-  ipcMain,
+    app,
+    globalShortcut,
+    BrowserWindow,
+    ipcMain,
 } = require('electron')
 const Screenshots = require('electron-screenshots').default
 const Baidu = require('./lib/direct-baidu')
 const FaasService = require('./lib/faas-service')
 const Menus = require('./lib/menus')
-const config = {
-  "direct": {
-    "host": "https://aip.baidubce.com",
-    "client_secret": "ADNU8yRYXluzLArkT7D7VxsMVaFjX6Qu",
-    "grant_type": "client_credentials",
-    "client_id": "zRS1pC2CfeYIfkjhxD8mthiM"
-  },
-  "proxy": {
-    "enable": true,
-    "host": "https://1613606417303976.cn-huhehaote.fc.aliyuncs.com/2016-08-15/proxy/serverless-hello-world/accurate/"
-  }
-}
+const config = require('./config/auth.json')
+
+
 
 const baidu = new Baidu(config.direct)
 const faasApi = new FaasService(config.proxy)
 
-async function directOcr(viewer) {
-  try {
-    if (!baidu.isExpired()) {
-      await baidu.getToken()
+// 直接调用百度api
+async function directOcr(image) {
+    try {
+        if (!baidu.isExpired()) {
+            await baidu.getToken()
+        }
+        let ocrCtx = await baidu.ocrTrans(image)
+        let imageText = null
+        if (ocrCtx && ocrCtx.words_result) {
+            imageText = baidu.transResult(ocrCtx.words_result)
+        }
+        return imageText
+    } catch (error) {
+        console.log('baiduApi:', error)
     }
-    let ocrCtx = await baidu.ocrTrans(viewer.dataURL)
-    let imageText = null
-    if (ocrCtx && ocrCtx.words_result) {
-      imageText = baidu.transResult(ocrCtx.words_result)
-    }
-    return imageText
-  } catch (error) {
-    console.log('baiduApi:', error)
-  }
 }
 
-async function faasOcr(viewer) {
-  try {
-    let ocrCtx = await  faasApi.accurate(viewer.dataURL)
-    console.log(ocrCtx)
-    let imageText = null
-    if (ocrCtx && ocrCtx.words_result) {
-      imageText = faasApi.transResult(ocrCtx.words_result)
+// 通过faas转发
+async function faasOcr(image) {
+    try {
+        let ocrCtx = await faasApi.accurate(image)
+        let imageText = null
+        if (ocrCtx && ocrCtx.words_result) {
+            imageText = faasApi.transResult(ocrCtx.words_result)
+        }
+        return imageText
+    } catch (error) {
+        console.log('faasApi:', error)
     }
-    return imageText
-  } catch (error) {
-    console.log('faasApi:', error)
-  }
 }
 
+// 创建窗口
 function createWindow() {
-  // 创建浏览器窗口
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true
-    },
-    icon: path.join(__dirname, 'images/logo.png')
-  })
+    // 创建浏览器窗口
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        },
+        icon: path.join(__dirname, 'images/logo.png')
+    })
 
-  // 并且为你的应用加载index.html
-  win.loadFile('index.html')
-  win.webContents.openDevTools()
+    // 并且为你的应用加载index.html
+    win.loadFile('index.html')
+    win.webContents.openDevTools()
 
-  return win
+    return win
 }
 
 
 app.on('ready', () => {
-  const win = createWindow()
-  const screenshots = new Screenshots()
-  const menus = new Menus(win, screenshots)
-  menus.createMenu()
-
-
-  // globalShortcut.register('ctrl+shift+f', () => screenshots.startCapture())
-  // ipcMain.on('asynchronous-message', (event, arg) => {
-  //   console.log(arg) // prints "ping"
-  //   event.sender.send('asynchronous-reply', 'pong')
-  // })
-
-  // ipcMain.on('synchronous-message', (event, arg) => {
-  //   console.log(arg) // prints "ping"
-  //   event.returnValue = 'pong'
-  // })
-
-  // 点击确定按钮回调事件
-  screenshots.on('ok', async (e, viewer) => {
-    if (viewer.dataURL) {
-      win.webContents.send('origin-image', viewer.dataURL)
-      let imageText = ''
-      if (config.proxy.enable) {
-        imageText = await faasOcr(viewer)
-      } else {
-        imageText = await directOcr(viewer)
-      }
-      win.webContents.send('imageText', imageText)
+    const win = createWindow()
+    const screenshots = new Screenshots()
+    const menus = new Menus(win, screenshots)
+    menus.createMenu()
+    console.log(config)
+    if (config.proxy && !config.proxy.enable) {
+        if (!(config.direct && config.direct.client_secret && config.direct.client_id)) {
+            win.webContents.send('setting', '非代理模式，请设置百度Ocr认证信息', '1')
+        }
     }
 
-  })
 
-  screenshots.on('cancel', e => {
-    // 执行了preventDefault
-    // 点击取消不会关闭截图窗口
-    e.preventDefault()
-    console.log('capture', 'cancel2')
-  })
-  // 点击保存按钮回调事件
-  screenshots.on('save', async (e, {
-    viewer
-  }) => {
-    if (viewer.dataURL) {
-      win.webContents.send('origin-image', viewer.dataURL)
-      let imageText = ''
-      if (config.proxy.enable) {
-        imageText = await faasOcr(viewer)
-      } else {
-        imageText = await directOcr(viewer)
-      }
-      win.webContents.send('imageText', imageText)
-    }
-  })
-  // debug({
-  //   showDevTools: true,
-  //   devToolsMode: 'undocked'
-  // })
+    ipcMain.on('image-drop', async (event, image) => {
+        if (config.proxy.enable) {
+            imageText = await faasOcr(image)
+        } else {
+            imageText = await directOcr(image)
+        }
+        win.webContents.send('imageText', imageText)
+
+    })
+
+    // 点击确定按钮回调事件
+    screenshots.on('ok', async (e, viewer) => {
+        if (viewer.dataURL) {
+            win.webContents.send('origin-image', viewer.dataURL)
+            let imageText = ''
+            if (config.proxy.enable) {
+                imageText = await faasOcr(viewer.dataURL)
+            } else {
+                imageText = await directOcr(viewer.dataURL)
+            }
+            win.webContents.send('imageText', imageText)
+        }
+
+    })
+
+    screenshots.on('cancel', e => {
+        // 执行了preventDefault
+        // 点击取消不会关闭截图窗口
+        // e.preventDefault()
+        console.log('capture', 'cancel2')
+    })
+    // 点击保存按钮回调事件
+    screenshots.on('save', async (e, {
+        viewer
+    }) => {
+        if (viewer.dataURL) {
+            win.webContents.send('origin-image', viewer.dataURL)
+            let imageText = ''
+            if (config.proxy.enable) {
+                imageText = await faasOcr(viewer.dataURL)
+            } else {
+                imageText = await directOcr(viewer.dataURL)
+            }
+            win.webContents.send('imageText', imageText)
+        }
+    })
+
+    // debug({
+    //   showDevTools: true,
+    //   devToolsMode: 'undocked'
+    // })
 })
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 })
 
 app.on('activate', () => {
-  // 在macOS上，当单击dock图标并且没有其他窗口打开时，
-  // 通常在应用程序中重新创建一个窗口。
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
-  }
+    // 在macOS上，当单击dock图标并且没有其他窗口打开时，
+    // 通常在应用程序中重新创建一个窗口。
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+    }
 })
--- views/js/index.js
+++ views/js/index.js
@@ -1,4 +1,7 @@
-const { ipcRenderer } = require('electron')
+const {
+	ipcRenderer
+} = require('electron')
+const fs = require('fs')
 const secretModel = require('./views/js/secret.js')
 
 let model = new secretModel('secret-model')
@@ -12,34 +15,106 @@ ipcRenderer.on('origin-image', (event, message) => {
 	document.getElementById('origin-image').src = message
 })
 
-ipcRenderer.on('setting', (event, message) => {
+ipcRenderer.on('setting', (event, m) => {
+	console.log(m)
+	m = JSON.parse(m)
 	let display = model.handler.style.display
-	if(display === "none"){
+	if (m && m.msg && !m.proxy) {
+		console.log(m.msg)
+	}
+
+	if (display === "" || display === "none") {
 		model.show()
 	}
+	if (m && m.proxy) {
+		$('input[type=radio][name="isProxy"][value="1"]').attr('checked', true)
+		$('input[type=text]').attr('disabled', true)
+		$('input[type=text]').val('')
+		$('.row.buttons').css('display', 'none')
+	} else {
+		$('.row.buttons').css('display', 'block')
+		$('input[type=radio][name="isProxy"][value="0"]').attr('checked', true)
+		if (m.client_secret && m.client_id) {
+			$('#client_secret').val(m.client_secret)
+			$('#client_id').val(m.client_id)
+		}
+	}
 })
+
+function imgTypeCheck(file) {
+	return !/.(jpg|jpeg|png|bmp|JPG|PNG|JPEG|BMP)$/.test(file.split('/')[1])
+}
+const holder = $('#image-wrap')
+holder.on("dragenter dragover", function (event) {
+	// 重写ondragover 和 ondragenter 使其可放置
+	event.preventDefault();
+	holder.addClass("holder-ondrag");
+});
+
+holder.on("dragleave", function (event) {
+	event.preventDefault();
+	holder.removeClass("holder-ondrag");
+});
+
+holder.on("drop", function (event) {
+	// 调用 preventDefault() 来避免浏览器对数据的默认处理（drop 事件的默认行为是以链接形式打开） 
+	event.preventDefault();
+	// 原生语句如下，但引进jquery后要更改
+	// var file=event.dataTransfer.files[0];
+	// 原因：
+	// 在jquery中，最终传入事件处理程序的 event 其实已经被 jQuery 做过标准化处理，
+	// 其原有的事件对象则被保存于 event 对象的 originalEvent 属性之中，
+	// 每个 event 都是 jQuery.Event 的实例
+	// 应该这样写:
+	var efile = event.originalEvent.dataTransfer.files[0];
+	console.log(efile)
+	if (efile.type && imgTypeCheck(efile.type)) {
+		fs.readFile(efile.path, function (err, data) {
+			if (err) throw err;
+			let base64 = Buffer.from(data).toString('base64');
+			let imgBase64 = `data:${efile.type};base64,${base64}`
+			$('#origin-image').attr('src', imgBase64)
+			$('#drop-tip').attr('display','none')
+			ipcRenderer.send('image-drop',imgBase64)
+		});
+	} else {
+		$('#drop-tip').text = `不支持该图片格式${efile.type}`
+	}
+	holder.removeClass("holder-ondrag");
+
+	return false;
+});
+
+
 
 $('input[type=radio][name="isProxy"]').change(() => {
 	let isProxy = $('input[type=radio][name="isProxy"]:checked').val()
-	if(isProxy === '1'){
-		$('input[type=text]').attr('disabled',true)
+	if (isProxy === '1') {
+		$('input[type=text]').attr('disabled', true)
 		$('input[type=text]').val('')
-	}else{
-		$('input[type=text]').attr('disabled',false)
+	} else {
+		$('.row.buttons').css('display', 'block')
+		$('input[type=text]').attr('disabled', false)
 		$('input[type=text]').val('')
 	}
 })
 
-$('.cancel').on('click',()=>{
+
+$('.cancel').on('click', () => {
 	$('input[type=text]').val('')
 	model.hide()
 })
 
-$('.ok').on('click',()=>{
+$('.ok').on('click', () => {
 	let checkResult = model.check()
-	if(checkResult){
+	if (checkResult) {
 		model.ok()
-	}else{
+	} else {
 		console.log('参数不正确')
 	}
+})
+
+$('.close').on('click', () => {
+	$('input[type=text]').val('')
+	model.hide()
 })
